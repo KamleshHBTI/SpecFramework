d3575f49811b602944b92552e546eb7216b59d63
Kamlesh Kumar <kamkumar@dminc.com>
2021-01-22 12:02:17 +0530

    Initial commit

---
+++ SpecFramework/SepcConfig.swift
@@ -0,0 +1,8 @@
+//
+//  SepcConfig.swift
+//  SpecFramework
+//
+//  Created by Kamlesh on 22/01/21.
+//
+
+import Foundation
---
+++ SpecFramework/SpecFramework.h
@@ -0,0 +1,18 @@
+//
+//  SpecFramework.h
+//  SpecFramework
+//
+//  Created by Kamlesh on 22/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpecFramework.
+FOUNDATION_EXPORT double SpecFrameworkVersionNumber;
+
+//! Project version string for SpecFramework.
+FOUNDATION_EXPORT const unsigned char SpecFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpecFramework/PublicHeader.h>
+
+
